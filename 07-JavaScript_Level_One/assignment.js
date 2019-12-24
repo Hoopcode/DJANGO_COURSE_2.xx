bdbf11527511f3a42a1f4a455b217275c6ef8cb4
Aiden Hooper <aidenmhooper@live.com>
2019-12-24 10:56:32 -0700

    Christmas Day Work

---
+++ 07-JavaScript_Level_One/assignment.js
@@ -0,0 +1,4 @@
+let inputNumber = input("Please enter the weight you want to convert");
+let convertedNumber = inputNumber * .454;
+alert("The weight in KG is: "+convertedNumber);
+console.log("Conversion Completed");
